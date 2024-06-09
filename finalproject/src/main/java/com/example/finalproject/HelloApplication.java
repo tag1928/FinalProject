7e839aeb76209646d82aea85027638b4feb31fda
tag1928 <www.tag1928@gmail.com>
2024-06-09 16:26:20 +0400

    Update HelloApplication.java

--- finalproject/src/main/java/com/example/finalproject/HelloApplication.java
+++ finalproject/src/main/java/com/example/finalproject/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("RegistrationPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setWidth(826);
         stage.setHeight(515);
